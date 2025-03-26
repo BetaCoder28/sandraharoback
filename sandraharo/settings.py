7d02949510de3386a3199cb40e4115882f11883a
BetaCoder28 <davidbaar07@gmail.com>
2025-03-26 17:16:12 -0600

    (Update) CORS MIDDLEWARES

--- sandraharo/settings.py
+++ sandraharo/settings.py
@@ -41,9 +41,12 @@ INSTALLED_APPS = [
     'user',
     'service',
     'citas',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +128,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOW_ALL_ORIGINS = False
+
+# Lista blanca de dominios permitidos
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",    # Frontend típico (React/Vue)
+    "http://127.0.0.1:3000",    # Alternativa IPv4
+    "http://localhost:5000",    # Otro servicio local
+    "http://127.0.0.1:5000",    # Alternativa IPv4
+]
